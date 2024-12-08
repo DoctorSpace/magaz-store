7803690404400449e934169402ca617bd6c8cbda
Melman Dmitriy <melman.dmitriy@wb.ru>
2024-12-09 02:38:34 +0300

    init

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import ReactLogo from "@shared/assets/react.svg?react";
+
+export const App = () => {
+  return (
+    <>
+      <div>
+        <h1>App</h1>
+        <ReactLogo />
+      </div>
+    </>
+  );
+};
